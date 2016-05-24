121b694d8472acee0f4840b7a8524fb815a9ea4a
Nicholas Johnson <nicholas@forwardadvance.com>
2016-05-24 12:46:21 +0100

    first commit

---
+++ demo/tests.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('being right about things', function() {
+
+});
+
+describe('maths', function() {
+
+  // it('should be the case that one and one is 2', function() {
+  //   expect(maths.div(2,2)).toBe(4);
+  // });
+  // it('should be the case that one and one is 2', function() {
+  //   expect(maths.div(2,2)).toBe(4);
+  // });
+});
